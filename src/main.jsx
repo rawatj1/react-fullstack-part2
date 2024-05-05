b67fb9007599dd504b48c222fe884fb3e5bce366
Jagdish Singh Rawat <jagdish.singhrawat@degruyter.com>
2024-05-05 22:46:59 +0200

    2.7: The Phonebook Step 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only Javascript',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are most important part of HTTP protocol',
-        important: true
-    }
-]
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App notes={notes} />
+    <App />
 )
